e33c1e5173d4a80b4719f96500f3621e76f8c57e
yiminghu <yimingdz@gmail.com>
2018-10-17 01:41:12 +0800

    add reminder script, useful when focusing desktop works

---
+++ reminder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from Foundation import NSUserNotification
+from Foundation import NSUserNotificationCenter
+from Foundation import NSUserNotificationDefaultSoundName
+import argparse
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(description=' -t TITLE -m MESSAGE')
+    parser.add_argument('-t', '--title', action='store', default='通知', type=lambda s: unicode(s, 'utf8'))
+    parser.add_argument('-m', '--message', action='store', default='...', type=lambda s: unicode(s, 'utf8'))
+    parser.add_argument('-d', '--delay', action='store', default=0)
+    parser.add_argument('--no-sound', action='store_false', default=True,
+        dest='sound')
+
+    args = parser.parse_args()
+    time.sleep(float(args.delay) * 60)
+    notification = NSUserNotification.alloc().init()
+    notification.setTitle_(args.title + " " + args.delay + u"分钟以前")
+    notification.setInformativeText_(args.message)
+    if args.sound:
+        notification.setSoundName_(NSUserNotificationDefaultSoundName)
+
+    center = NSUserNotificationCenter.defaultUserNotificationCenter()
+    center.deliverNotification_(notification)
+
+
+if __name__ == '__main__':
+    main()
+
